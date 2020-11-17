86c5c226cdb5f09ca2a50c80771c6b012e5ec43d
jeremiahtorralba-sheridancollege <71898842+jeremiahtorralba-sheridancollege@users.noreply.github.com>
2020-11-16 23:23:13 -0500

    Update Card.java

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -1,8 +1,3 @@
-/**
- * SYST 17796 Project Base code.
- * Students can modify and extend to implement their game.
- * Add your name as an author and the date!
- */
 package ca.sheridancollege.project;
 
 /**
@@ -12,17 +7,72 @@ package ca.sheridancollege.project;
  * @modifier AhmadBustani
  * @modifier JaykumarChaudhari
  * @modifier ElmerAlmeida
- * @modifier JermiahTorralba
+ * @modifier JeremiahTorralba
  */
-public abstract class Card {
-    //default modifier for child classes
 
-    /**
-     * Students should implement this method for their specific children classes
-     *
-     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
-     */
+public class Card {
+    private int value;
+    private Suits suit;
+    
+    //constructor to create a card with given suit and value
+    Card(int cValue, Suits cSuit) {
+         suit = cSuit;
+         value = cValue;
+     }
+    
+    //Overriding the toString method to print card type in format ex. "Ace of Spades"
     @Override
-    public abstract String toString();
-
+    public String toString(){
+        String name;
+        
+        //switch case value of card
+        switch(value) {
+            case 1:
+                name = "Ace";
+                break;
+            
+            case 11:
+                name = "Jack";
+                break;
+            
+            case 12:
+                name = "Queen";
+                break;
+                
+            case 13:
+                name = "King";
+                break;
+                
+            default:
+                //for values above 13 (king) and between 2 and 10 where value name is integer to string
+                name = new Integer(value).toString();
+                break;
+        }
+        
+        //add " of <suit>" to the end of the name
+        name = name.concat(" of " + suit.toString());       
+        
+        return name;
+    }
+    
+    //accessors and mutators;
+    public int getValue() {
+        return value;
+    }
+    
+    public void setValue(int cardValue) {
+       value = cardValue;
+    }
+    
+    public Suits getSuit (){
+        return suit;
+    }    
+       
+    public int compareTo(Card card2) {
+        //convert values to Integers to use compareTo method
+        Integer a = value;
+        Integer b = card2.getValue();
+        
+        return a.compareTo(b);
+    }
 }
