6778ae8b26a2e5e1ab984646c0131b6dcb039a06
jeremiahtorralba-sheridancollege <71898842+jeremiahtorralba-sheridancollege@users.noreply.github.com>
2020-11-16 23:24:46 -0500

    Update Player.java

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -1,8 +1,3 @@
-/**
- * SYST 17796 Project Base code.
- * Students can modify and extend to implement their game.
- * Add your name as an author and the date!
- */
 package ca.sheridancollege.project;
 
 /**
@@ -11,41 +6,34 @@ package ca.sheridancollege.project;
  * @modifier AhmadBustani
  * @modifier JaykumarChaudhari
  * @modifier ElmerAlmeida
- * @modifier JermiahTorralba
+ * @modifier JeremiahTorralba
  */
-public abstract class Player {
 
-    private String name; //the unique name for this player
+public abstract class Player {
+    //private String nName; //the unique name for this player
 
     /**
      * A constructor that allows you to set the player's unique ID
      *
      * @param name the unique ID to assign to this player.
      */
-    public Player(String name) {
-        this.name = name;
-    }
+    //public Player(String nName) {
+        //this.nName = nName;
+    //}
 
-    /**
-     * @return the player name
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * Ensure that the playerID is unique
-     *
-     * @param name the player name to set
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    /**
-     * The method to be overridden when you subclass the Player class with your specific type of Player and filled in
-     * with logic to play your game.
-     */
-    public abstract void play();
-
+//    /**
+//     * @return the player name
+//     */
+//    public String getName() {
+//        return nName;
+//    }
+//
+//    /**
+//     * Ensure that the playerID is unique
+//     *
+//     * @param name the player name to set
+//     */
+//    public void setName(String nName) {
+//        this.nName = nName;
+//    }
 }
