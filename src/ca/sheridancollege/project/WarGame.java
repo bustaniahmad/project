f57d61a80ac9363b14a193755a479553d19f5bcd
jeremiahtorralba-sheridancollege <71898842+jeremiahtorralba-sheridancollege@users.noreply.github.com>
2020-12-08 00:30:38 -0500

    Update WarGame.java

--- src/ca/sheridancollege/project/WarGame.java
+++ src/ca/sheridancollege/project/WarGame.java
@@ -8,71 +8,75 @@ package ca.sheridancollege.project;
  * @modifier ElmerAlmeida
  * @modifier JeremiahTorralba
  */
-public abstract class WarGame extends Game {
-    //protected data fields accessible to subclasses
-    private String GameName = "War";
-    protected WarPlayer Player1;
-    protected WarPlayer Player2;
+public abstract class WarGame extends Game{
+    //data fields protected to be accessible to subclasses
+    protected final String GAMENAME = "War";
+    protected WarPlayer player1;
+    protected WarPlayer player2;
     protected boolean inPlay;
     protected Deck deck;
     protected String scorecard;
     protected int stake;
     
-    //Create a War game constructor
-    public WarGame(String GameName, WarPlayer nPlayer1, WarPlayer nPlayer2, boolean inPlay, Deck deck, String scorecard, int stake) {   
-        this.GameName = GameName;
-        this.Player1 = nPlayer1;
-        this.Player2 = nPlayer2;
-        this.inPlay = false;
-        this.deck = null;
-        this.scorecard = "";
-        this.stake = 0;       
-    }
-   
-    // setup() War game method
-    public abstract void setup();
-    /**
-    * Play the game. This might be one method or many method calls depending on your game.
-    * play() moved from Player class
-    */
-    public abstract void play();
-    
-    // Name of Game access
-    public String getGameName() {
-        return GameName;
+    //constructor to create a game
+    public WarGame(WarPlayer nPlayer1, WarPlayer nPlayer2) {
+        player1 = nPlayer1;
+        player2 = nPlayer2;
     }
     
-    // Player 1 access
+    public WarGame() {}
+    
+    public String getGamename() {
+        return GAMENAME;
+    }
+    
     public WarPlayer getPlayer1() {
-        return Player1;
+        return player1;
     }
     
-    // Player 1 set
     public void setPlayer1(WarPlayer p1) {
-        Player1 = p1;
+        player1 = p1;
     }
     
-    // Player 2 access
     public WarPlayer getPlayer2() {
-        return Player2;
+        return player2;
     }
     
-    // Player 2 set
     public void setPlayer2(WarPlayer p2) {
-        Player2 = p2;
+        player2 = p2;
     }
     
-    // Check if Game is in play status
     public boolean inPlay() {
         return inPlay;
     }
     
-    // Set Game in play status
     public void setInPlay(boolean play) {
         inPlay = play;
     }
     
-    // War match situation where cards are the same hierarchy in War Game
     public abstract void warMatch();
-        
+    
+    public void declareMatchWinner(WarPlayer winner) {
+        System.out.println("\n" + winner.getName() + " wins the match and gained " + stake + " point(s)!");
+    }
+
+    public void declareWinner(WarPlayer winner, WarPlayer loser) {
+        System.out.println("\nCongratulations " + winner.getName() + "! You won the game with a score of " +
+                winner.getScore() + "! " + loser.getName() + " lost the game with just " + loser.getScore() + " points.");
+    }
+    
+    public void declareTie() {
+        System.out.println("\nStalemate! Both players have the same score!");
+    }
+    
+    public void showScorecard() {
+        System.out.println("SCORES:");
+        System.out.println(player1.getName() + "'s score: " + player1.getScore());
+        System.out.println(player2.getName() + "'s score: " + player2.getScore());
+        System.out.println("Flips left: " + (deck.cardsLeft() / 2) + "\n");
+    }
+    
+    public abstract void setup();
+    
+    public abstract void play();
 }
