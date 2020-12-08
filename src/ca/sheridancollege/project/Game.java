0df5a11e1164721e489bccd1fc71a09b85b72fbc
jeremiahtorralba-sheridancollege <71898842+jeremiahtorralba-sheridancollege@users.noreply.github.com>
2020-12-08 00:27:52 -0500

    Update Game.java

--- src/ca/sheridancollege/project/Game.java
+++ src/ca/sheridancollege/project/Game.java
@@ -12,34 +12,50 @@ import java.util.ArrayList;
  * @modifier JeremiahTorralba
  */
 
-public abstract class Game {
-//    private final String name;//the title of the game
-//    private ArrayList<Player> players;// the players of the game
-//
-//    public Game(String name) {
-//        this.name = name;
-//        players = new ArrayList();
-//    }
-//
-//     /**
-//     * @return the name
-//     */
-//    public String getName() {
-//        return name;
-//     }
-//
-//     /**
-//     * @return the players of this game
-//     */
-//     public ArrayList<Player> getPlayers() {
-//        return players;
-//     }
-//
-//     /**
-//      * @param players the players of this game
-//     */
-//     public void setPlayers(ArrayList<Player> players) {
-//        this.players = players;
-//     }
-//
-}
+public abstract class Game 
+{
+    private final String gameName;//the title of the game
+    private ArrayList <Player> players;// the players of the game
+    
+    public Game(String givenName)
+    {
+        gameName = givenName;
+        players = new ArrayList();
+    }
+    
+    //added to implement a wargame subclass
+    public Game() {
+        gameName = "";
+    }
+
+    /**
+     * @return the gameName
+     */
+    public String getGameName() 
+    {
+        return gameName;
+    }
+    
+     /**
+     * @return the players of this game
+     */
+    public ArrayList <Player> getPlayers() 
+    {
+        return players;
+    }
+
+    /**
+     * @param players the players of this game
+     */
+    public void setPlayers(ArrayList <Player> players) 
+    {
+        this.players = players;
+    }
+    
+    /**
+     * Play the game. This might be one method or many method calls depending
+     * on your game.
+     */
+    public abstract void play();
+  
+}//end class
